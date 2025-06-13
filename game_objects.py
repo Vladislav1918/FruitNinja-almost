f1749d4ba67df3d69021565d6bdaddce4bb6ba1d
unknown <vladok111@icloud.com>
2025-06-13 18:11:22 +0300

    13.06.25

--- game_objects.py
+++ game_objects.py
@@ -1,6 +1,8 @@
 import math
 import random
 import pygame
+
+pygame.init()
 class Fruits:#создаем класс
     def __init__(self,x,y,image, screen_width, screen_height):#КОНСТРУКТОР класса - указываеи обязательные параметры при создантт обьекта      self - общее название для всех обьеков класса
         # Уникальные параметры - у каждого обьекта они разные
@@ -13,6 +15,7 @@ class Fruits:#создаем класс
         self.screen_width = 1900
         self.screen_height = 1000
         self.active = False#сохраняем активность обьекта
+        self.current_fruit = None# в пременную сохраняется текущий фрукт
         self.Vx = 0#сохраняем горизонтальную  скорость обьекта
         self.Vy = 0#сохраняем вертекальную  скорость обьекта
         #self.rect = image.get_rect()
@@ -22,14 +25,14 @@ class Fruits:#создаем класс
         self.fruit_types = [
             {
                 "name": "watermelon",
-                "whole": pygame.image.load("Fruits/целый_арбуз.png").convert_alpha(),  # Здесь будет изображение целого арбуза
-                "left_half": pygame.image.load("Fruits/левая_половинка.png").convert_alpha(),  # Здесь будет изображение левой половинки
-                "right_half": pygame.image.load("Fruits/правая_половинка_арбуза.png").convert_alpha(),  # Здесь будет изображение правой половинки
+                "whole": pygame.image.load("Fruits/целый_арбуз.png"),  # Здесь будет изображение целого арбуза
+                "left_half": pygame.image.load("Fruits/левая_половинка.png"),  # Здесь будет изображение левой половинки
+                "right_half": pygame.image.load("Fruits/правая_половинка_арбуза.png"),  # Здесь будет изображение правой половинки
                 "points": 1
             },
             {
                 "name": "apple",
-                "whole": pygame.image.load("Fruits/apple(1).png"),
+                "whole": pygame.image.load("Fruits/apple (1).png"),
                 "left_half": pygame.image.load("Fruits/left_part_of_apple.png"),
                 "right_half": pygame.image.load("Fruits/right_part_of_apple.png"),
                 "points": 1
@@ -46,9 +49,11 @@ class Fruits:#создаем класс
         self.Vy = speed * math.sin(angle)# Рассчитываем скорость полета
     def create_fruits(self):
         random_fruits = random.choice(self.fruit_types)# метод random.choice позволяет взять рандомный параметр из списка
+        self.current_fruit = random_fruits
         self.active = True
         self.x = random.randint(400, self.screen_width - 400)
         self.y = 1000
+        self.image = random_fruits["whole"]
         angle = random.uniform(math.radians(70), math.radians(110))  # сохраняем угол палета обьекта
         speed = random.uniform(1000, 1200)
         self.Vx = speed * math.cos(angle)  # Рассчитываем скорость полета
@@ -61,17 +66,9 @@ class Fruits:#создаем класс
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-arbuz = Fruits(x=0,y=0,image=1)# создал обьект класса
+arbuz = Fruits(x=0,y=0,image=1, screen_width=100,screen_height = 100)# создал обьект класса
 print(arbuz.image)# выводим значение image у обьекта arbuz
+
+arbuz.create_fruits()# применяем метод класса create_fruits для обьекта класаа arbuz
+print(arbuz.current_fruit)
 
